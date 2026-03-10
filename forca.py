86a34b41269ff87f569ca71b6f1339a8fb020692
lettitu <leticia.pereira.araujo@escola.pr.gov.br>
2026-03-10 11:48:27 -0300

    Add files via upload

--- forca.py
+++ forca.py
@@ -5,7 +5,20 @@ def jogar():
 
     palavra_secreta = "banana"
 
-    while()
+    enforcou = False
+    acertou = False
+
+    while(not enforcou and not acertou):
+
+        chute = input("Qual letra? ")
+
+        index = 0
+        for letra in palavra_secreta:
+            if(chute == letra):
+                print("Encontrei a letra {} na posição {} ". format(letra, index))
+            index = index + 1    
+
+        print("Jogando...")
 
     print("Fim do jogo")
 
